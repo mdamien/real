e2e3ccf23314c3e0f4d4dc6b7a4d2469669c6dac
Damien <damien@dam.io>
2015-03-04 13:20:47 +0100

    initial demo

---
+++ js/box_utils.js
@@ -0,0 +1,58 @@
+var   b2Vec2 = Box2D.Common.Math.b2Vec2
+,    b2AABB = Box2D.Collision.b2AABB
+,    b2BodyDef = Box2D.Dynamics.b2BodyDef
+,    b2Body = Box2D.Dynamics.b2Body
+,    b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+,    b2Fixture = Box2D.Dynamics.b2Fixture
+,    b2World = Box2D.Dynamics.b2World
+,    b2MassData = Box2D.Collision.Shapes.b2MassData
+,    b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
+,    b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+,    b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+,    b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef;
+
+function addLine(start,end) {
+    var fixDef = new b2FixtureDef;
+    fixDef.shape = new b2PolygonShape;
+    fixDef.density = 1.0;
+    fixDef.friction = 0.5;
+    fixDef.restitution = .5;
+    fixDef.shape.SetAsArray([
+                start,
+                end],2
+                );
+    var bodyDef = new b2BodyDef;
+    bodyDef.type = b2Body.b2_staticBody;
+    bodyDef.position.Set(0,0);
+    rbData = new Object();
+    rbData.name = "Line";
+    rbData.start = start;
+    rbData.end = end;
+    bodyDef.userData = rbData;
+    var line = world.CreateBody(bodyDef).CreateFixture(fixDef);
+    return line;
+}
+
+function addDynCircle(x,y){
+    var fixDef = new b2FixtureDef;
+    fixDef.density = 1.0;
+    fixDef.friction = 0.5;
+    fixDef.restitution = 0.2;
+
+    var bodyDef = new b2BodyDef;
+    bodyDef.type = b2Body.b2_dynamicBody;
+    if(Math.random() > 0.5) {
+       fixDef.shape = new b2PolygonShape;
+       fixDef.shape.SetAsBox(
+             Math.random() + 0.1 //half width
+          ,  Math.random() + 0.1 //half height
+       );
+    } else {
+       fixDef.shape = new b2CircleShape(
+          Math.random() + 0.1 //radius
+       );
+    }
+    bodyDef.position.x = x;
+    bodyDef.position.y = y;
+    world.CreateBody(bodyDef).CreateFixture(fixDef);
+}
